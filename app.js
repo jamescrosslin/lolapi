a5c66e0f7de78bf4e415fa6940908944ffce6f5a
James Crosslin <jamesmscrosslin@gmail.com>
2021-03-06 15:22:49 -0800

    adding heroku functionality

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.get("/:id", async (req, res) =>{
   res.json(data)
 })
 
-app.listen(3000, ()=>{
-  console.log("Server is listening on port 3000")
-})
+let port = process.env.PORT
+if (port == null || port == "") {
+  port = 8000
+  console.log(`Listening on port 8000!`)
+}
+app.listen(port)
