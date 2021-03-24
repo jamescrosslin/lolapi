22d8b3d957f77699680b93633fca99553e672dc7
James Crosslin <jamesmscrosslin@gmail.com>
2021-03-23 17:41:37 -0700

    changed app to try to get it working

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const app = express();
 
-const express = require("express")
-const fetch = require("node-fetch")
-const app = express()
-
-
-
-app.get("/:id", async (req, res) =>{
-  const lolurl =
-  `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${req.params.id}?api_key=RGAPI-cfb256f7-e89d-43ed-86f5-710adb74eeea`;
+app.get("/:id", async (req, res) => {
+  const lolurl = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${req.params.id}?api_key=RGAPI-cfb256f7-e89d-43ed-86f5-710adb74eeea`;
   const response = await fetch(lolurl);
   const data = await response.json();
-  res.json(data)
-})
+  res.json(data);
+});
 
-let port = process.env.PORT
-if (port == null || port == "") {
-  port = 8000
-  console.log(`Listening on port 8000!`)
-}
-app.listen(port)
+let port = process.env.PORT || 8000;
+
+app.listen(port, () => `Listening on port ${port}`);
